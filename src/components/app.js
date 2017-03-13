f10dc58650656933e8d1e3c2e1b031a18f056c53
Felipe Maccheri <felipemaccheri@gmail.com>
2017-03-13 10:12:34 -0300

    Creating add new todo input

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,49 @@ import List from './list';
 import _ from 'lodash';
 
 const todoList = [
-    { id: 1, task: 'Watch TV', isCompleted: false },
-    { id: 2, task: 'Wash the dishes', isCompleted: true }
+    {
+        id: 1,
+        task: 'Watch TV',
+        isCompleted: false
+    }, {
+        id: 2,
+        task: 'Wash the dishes',
+        isCompleted: true
+    }
 ];
+
+class AddItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.doSomething = this.doSomething.bind(this);
+    }
+
+    doSomething(e) {
+        e.preventDefault();
+        console.log(this.props);
+    }
+
+    render() {
+        const divStyle = {
+            marginTop: 10
+        };
+
+        return (
+            <div className='col-sm-offset-2 col-md-8' style={divStyle}>
+                <form className='form-horizontal' onSubmit={this.doSomething}>
+                    <div className='form-group'>
+                        <label htmlFor='addNewTodo' className='col-sm-2 control-label'>Add new todo</label>
+                        <div className='col-md-8'>
+                            <input type="text" className='form-control' id='addNewTodo' placeholder='Type your todo here'/>
+                        </div>
+                        <button className='btn btn-primary' onClick={this.doSomething}>Add</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
     constructor(props) {
@@ -19,22 +59,20 @@ class App extends Component {
     }
 
     onTodoItemClick(id) {
-      const foundTodo = _.find(this.state.todos, todo => todo.id === id);
-      foundTodo.isCompleted = !foundTodo.isCompleted;
+        const foundTodo = _.find(this.state.todos, todo => todo.id === id);
+        foundTodo.isCompleted = !foundTodo.isCompleted;
 
-      this.setState({ todos: this.state.todos });
+        this.setState({todos: this.state.todos});
     }
 
-    handleEdit(id, newTaskName){
-      const editedTodo = _.find(this.state.todos, todo => todo.id === id);
-      editedTodo.task = newTaskName;
+    handleEdit(id, newTaskName) {
+        const editedTodo = _.find(this.state.todos, todo => todo.id === id);
+        editedTodo.task = newTaskName;
 
-      this.setState({ todos: this.state.todos });
+        this.setState({todos: this.state.todos});
     }
 
-    handleDelete(){
-
-    }
+    handleDelete() {}
 
     render() {
         return (
@@ -46,11 +84,8 @@ class App extends Component {
                     </div>
                 </div>
                 <div className='row'>
-                    <List
-                      todos={this.state.todos}
-                      toggleTask={this.onTodoItemClick.bind(this)}
-                      handleEdit={this.handleEdit.bind(this)}
-                    />
+                    <AddItem todos={this.state.todos}/>
+                    <List todos={this.state.todos} toggleTask={this.onTodoItemClick.bind(this)} handleEdit={this.handleEdit.bind(this)}/>
                 </div>
             </div>
         );
